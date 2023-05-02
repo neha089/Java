d36e8247b376722a79d89a5af1a6806286e7cfcc
neha089 <nehapatel30325@gmail.com>
2023-05-02 20:37:16 +0530

    commit all the java files

--- output/ac10.java
+++ output/ac10.java
@@ -1,3 +1,56 @@
+// //TODO: list of names
+// package output;
+// import java.util.*;
+// public class ac10 {
+//     public static void main(String[] args){
+//         Scanner s= new Scanner(System.in);
+//         int size;
+//         size=s.nextInt();
+//         String name []=new String[size];
+//         for(int i=0;i<size;i++){
+//           name[i]=s.nextLine();
+//         }
+//         for(int i=0;i<size;i++){
+//             System.out.print(name[i]);
+//           }
+
+//     }
+
+
+    
+// }
+//TODO: find max and min
+// package output;
+// import java.util.*;
+// public class ac10 {
+//     public static void main(String[] args){
+//         Scanner s= new Scanner(System.in);
+//         int size;
+//         size=s.nextInt();
+//         int arr []=new int [size];
+//         System.out.println("enter elements for your array");
+//         for(int i=0;i<size;i++){
+//           arr[i]=s.nextInt();
+//         }
+//         System.out.println("your array is : ");
+//         for(int i=0;i<size;i++){
+//             System.out.print(arr[i]);
+//           }
+//           int max=arr[0];
+//           int min=arr[0];
+//           for(int i=0;i<size;i++){
+//             if(arr[i]<min)
+//             min=arr[i];
+//             if(arr[i]>max)
+
+//             max=arr[i];
+//           }
+//           System.out.println("your max is "+max);
+//           System.out.println("your min is"+min);
+
+//     }
+//   }
+//TODO: check if array is sorted or not
 package output;
 import java.util.*;
 public class ac10 {
@@ -5,15 +58,29 @@ public class ac10 {
         Scanner s= new Scanner(System.in);
         int size;
         size=s.nextInt();
-        String name []=new String[size];
+      int a []=new int[size];
+      System.out.println("enter elements");
         for(int i=0;i<size;i++){
-          name[i]=s.nextLine();
+          a[i]=s.nextInt();
         }
+        System.out.println("your array is");
         for(int i=0;i<size;i++){
-            System.out.print(name[i]);
+            System.out.print(a[i]);
           }
+          boolean x=true;
+          for(int i=0;i<size-1;i++){
+            
+              if(a[i]>a[i+1]){
+              x=false;
+              break;
+              }
+            
+          }
+          System.out.println(x);
+          if(x)
+          System.out.println("its sorted");
+          else
+          System.out.println("not sorted");
 
     }
-
-    
-}
+  }
